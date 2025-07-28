347b7aa22dfc0932c5fd8e9096fcfe3a54c6802d
cabbage <cab226023@gmail.com>
2025-07-28 21:09:35 +0900

    383: hash count str

--- 383/1.py
+++ 383/1.py
@@ -1,3 +1,15 @@
+from collections import Counter
+
+'''
+how to use counter:
+a=Counter('abcddeea')
+print(a)
+>> Counter({'a': 2, 'd': 2, 'e': 2, 'b': 1, 'c': 1})
+print(a['a'])
+>> 2
+'''
+
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         '''
@@ -11,5 +23,20 @@ class Solution:
             idx = ord(char)-97
             arr[idx] -= 1
             if arr[idx] < 0:
+                return False
+        return True
+
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        '''
+        8min, counter
+        '''
+        m = Counter(magazine)
+        r = Counter(ransomNote)
+        return len(r-m) < 1
+
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        '''simplify the hashtable version'''
+        for char in set(ransomNote):
+            if ransomNote.count(char) > magazine.count(char):
                 return False
         return True
