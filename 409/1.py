d0f67f176d1358180a5fda568905dd9821435ea3
cabbage <cab226023@gmail.com>
2025-08-29 16:56:20 +0900

    409:hashmap

---
+++ 409/1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        '''
+        16:15-
+        cur,center
+        # cur, center, center_len = 0, 0, 0
+        # ss = '#'+'#'.join(s)+'#'
+        # print(ss)
+        # n = len(ss)
+        # max_len = [0]*n
+        # for i in range(n):
+        #     l, r = i-1, i+1
+        #     expand = 0
+        #     while l > -1 and r < n:
+        #         expand += 1
+        #         l -= 1
+        #         r += 1
+        i misunderstood the question...
+        '''
+        # 16:45-16:50
+        # if all can paired, return n
+        # if has remain:
+        #   only one remain: n
+        #   otherwise, n-len(chars)+1
+        chars = set()
+        n = len(s)
+        for c in s:
+            if c in chars:
+                chars.remove(c)
+            else:
+                chars.add(c)
+        if not chars:  # all paired
+            return n
+        return n-len(chars)+1  # use one odd as center, others are wasted
+
+
+t = "abccccdd"
+r = Solution().longestPalindrome(t)
+print(r)
