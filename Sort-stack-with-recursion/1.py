2ffd84b17f6e7f0c06c6ec4d8d1ede076c801abc
cabbage <cab226023@gmail.com>
2025-10-18 11:43:48 +0900

    dfs: sort stack with recursion(bubble sort like

---
+++ Sort-stack-with-recursion/1.py
@@ -0,0 +1,31 @@
+'''
+Sort the stack in ascending order (smallest element at the bottom and largest at the top).
+'''
+
+
+def SortStackWithRecursion(stack: list[int]):
+    def rec(s: list[int]):
+        if not s:
+            return
+        elem = s.pop()
+        rec(s)
+        insert(s, elem)
+    rec(stack)
+    return stack
+
+
+def insert(s: list[int], elem: int):
+    '''given a sorted s, insert'''
+    if not s or s[-1] < elem:
+        s.append(elem)
+        return
+    cur_num = s.pop()
+    insert(s, elem)
+    s.append(cur_num)
+
+
+t = [1, 2, 3]
+t = [3, 2, 1]
+t = [41, 3, 32, 2, 11]
+r = SortStackWithRecursion(t)
+print(r)
